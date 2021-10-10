73967fbc2fcc8ce0a8d9bd369779215c6515d681
Remelson <rsmagatca@outlook.com>
2021-10-10 13:47:30 -0500

    Generated 'Category' and 'Breed' model's controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'breeds/index'
+  get 'breeds/show'
+  get 'categories/index'
+  get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
