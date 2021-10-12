95facdd9036aa4ef9756bf0467a54f87bcb7b6b3
Remelson <rsmagatca@outlook.com>
2021-10-11 19:40:45 -0500

    Updated 'Category' and 'Breed' model's validations

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,6 +1,6 @@
 class Breed < ApplicationRecord
     belongs_to :category
 
-    validates :breed_name, :image_url, presence: true
+    validates :breed_name, :image_url, :breed_type, presence: true
     validates :breed_name, length: { maximum: 100 }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     has_many :breeds, dependent: :destroy
 
-    validates :category_name, presence: true
+    validates :category_name, :breed_type, presence: true
     validates :category_name, length: { maximum: 100 }
 end
