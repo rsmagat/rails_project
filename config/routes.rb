52c297063d6089889ceda96f09ad4353af7de43b
Renz <rsmagatca@outlook.com>
2021-10-18 02:06:53 -0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 #   get "/breeds", to: "breeds#index", as: "breeds"
 #   get "/breeds/:id", to: "breeds#show", as: "breed"
+  root "breeds#index"
   get "/categories", to: "categories#index", as: "categories"
   get "/categories/:id", to: "categories#show", as: "category"
   get "/about", to: "about#index", as: "about"
